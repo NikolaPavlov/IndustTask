8273c6af066e80b9691576ffdae848ff5a6a2a01
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2019-12-11 20:27:20 +0200

    sync functionality on button hit achived

--- ExchangeProject/exchange/views.py
+++ ExchangeProject/exchange/views.py
@@ -1,14 +1,16 @@
-from django.http import JsonResponse, HttpResponse
+from decimal import Decimal
+
+import requests
+
+from bs4 import BeautifulSoup
+
+from django.http import HttpResponse, JsonResponse
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic.list import ListView
 
 from .forms import AddCurrencyForm
 from .models import Currency
-
-
-from bs4 import BeautifulSoup
-import requests
 
 
 class AllCurrenciesListView(ListView):
@@ -54,7 +56,32 @@ def scraper(request):
     html_doc = r.text
     soup = BeautifulSoup(html_doc, "html.parser")
 
-    # table = soup.find('table')
-    # rows = soup.find_all('tr')
+    table = soup.find('table', attrs={'class': 'table'})
+    table_body = table.find('tbody')
+    currency_data = html_table_body_to_array(table_body)
 
-    return HttpResponse(soup)
+    for row in currency_data:
+        rate_vs_base = Decimal(row[3])
+
+        quantity = int(row[2])
+        if quantity != 1:
+            rate_vs_base = rate_vs_base / quantity
+
+        obj, created = Currency.objects.update_or_create(full_name=row[0],
+                                                         currency_abbr=row[1],
+                                                         defaults={
+                                                            'rate_vs_base': rate_vs_base
+                                                         })
+
+    return redirect(reverse('exchange:index'))
+
+
+def html_table_body_to_array(table_body):
+    # return array with the data of the table
+    data = []
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])  # Get rid of empty values
+    return data[:-1]  # The last row of the table is removed
